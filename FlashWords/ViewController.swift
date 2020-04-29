c7f64254b4c64ab245e2f4b73709d2de160db85c
Jędrzej Kuś <jedrzej.kus@icloud.com>
2020-04-29 19:14:10 +0200

    Initial Commit

---
+++ FlashWords/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FlashWords
+//
+//  Created by Jędrzej Kuś on 29/04/2020.
+//  Copyright © 2020 Jędrzej Kuś. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
